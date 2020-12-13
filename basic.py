56539407139a5f15b43843962f9c19bf6546f25e
AlexP <57384789+1meastro@users.noreply.github.com>
2020-12-12 19:30:48 -0500

    Comment section

---
+++ basic.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("example.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
